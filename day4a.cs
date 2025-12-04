ee982045a558fcb7942c1cadda4c5686d2563d1e
Ben Heldoorn <benheldoorn@outlook.com>
2025-12-04 18:48:52 +0100

    day 4

---
+++ day4a.cs
@@ -0,0 +1,28 @@
+var map = System.IO.File.ReadAllLines("4a.input")
+    .Select(line => line.Select(c => c == '@').ToArray())
+    .ToArray();
+
+var directions = new (int dx, int dy)[] { (0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (1, -1), (-1, 1), (1, 1) };
+
+var maxX = map[0].Length;
+var maxY = map.Length;
+
+bool get(int x, int y)
+{
+    return (x >= 0 && x < maxX && y >= 0 && y < maxY) ? map[y][x] : false;
+}
+
+var count = 0;
+for (int y = 0; y < maxY; y++)
+{
+    for (int x = 0; x < maxX; x++)
+    {
+        var free = directions.Count(d => !get(x + d.dx, y + d.dy));
+        if (free > 4 && get(x, y))
+        {
+            count++;
+        }
+    }
+}
+
+System.Console.WriteLine(count);
