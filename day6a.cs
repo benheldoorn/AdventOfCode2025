4727ffd5ed4ec30d6b7dfea1c6ea2e8f85fb40fe
Ben Heldoorn <benheldoorn@outlook.com>
2025-12-07 10:31:06 +0100

    day 6

---
+++ day6a.cs
@@ -0,0 +1,39 @@
+var input = File.ReadAllLines("6a.input");
+
+var operands = input[..^1]
+    .Select(line => line
+        .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse)
+        .ToList())
+    .ToList();
+
+var operaters = input[^1]
+    .Where(c => c == '+' || c == '*')
+    .ToList();
+
+long total = 0;
+
+for (int i = 0; i < operaters.Count; i++)
+{
+    var @operator = operaters[i];
+    long result = @operator == '*' ? 1 : 0;
+
+    for (int j = 0; j < operands.Count; j++)
+    {
+        var operand = operands[j][i];
+        
+        if (@operator == '+')
+        {
+            result += operand;
+        }
+
+        if (@operator == '*')
+        {
+            result *= operand;
+        }
+    }
+
+    total += result;
+}
+
+Console.WriteLine(total);
---
+++ day6b.cs
@@ -0,0 +1,65 @@
+var input = File.ReadAllLines("6a.input")
+    .Select(line => line.ToCharArray())
+    .ToArray();
+
+var rowLength = input[0].Length;
+var columnLength = input.Length;
+
+var rotated = new char[rowLength][];
+for (var i = 0; i < rowLength; i++)
+{
+    rotated[i] = new char[columnLength];
+}
+
+for (var row = 0; row < columnLength; row++)
+{
+    for (var column = 0; column < rowLength; column++)
+    {
+        rotated[column][row] = input[row][column];
+    }
+}
+
+var concatenated = rotated
+    .Select(chars => new string(chars))
+    .ToList();
+
+concatenated.Add(string.Empty);
+
+var total = 0L;
+var @operator = ' ';
+var operands = new List<long>();
+foreach (var line in concatenated)
+{
+    if (string.IsNullOrWhiteSpace(line))
+    {
+        if (@operator == '+')
+        {
+            total += operands.Sum();
+        }
+
+        if (@operator == '*')
+        {
+            total += operands.Aggregate(1L, (acc, val) => acc * val);
+        }
+
+        operands.Clear();
+        @operator = ' ';
+        continue;
+    }
+
+    if (line.Contains('*'))
+    {
+        @operator = '*';
+    }
+
+    if (line.Contains('+'))
+    {
+        @operator = '+';
+    }
+
+    operands.Add( long.Parse(line.Replace('*', ' ')
+        .Replace('+', ' ')));
+}
+
+
+Console.WriteLine(total);
