baebf2240ff89de3723aafe72295fc3b1394f41d
Ben Heldoorn <benheldoorn@outlook.com>
2025-12-09 20:43:27 +0100

    day 8

---
+++ day8a.cs
@@ -0,0 +1,76 @@
+var junctions = File.ReadAllLines("8a.input")
+    .Select(line => line.Split(","))
+    .Select(parts => new Coordinate(
+        X: int.Parse(parts[0]),
+        Y: int.Parse(parts[1]),
+        Z: int.Parse(parts[2])))
+    .ToList();
+
+var connections = junctions
+    .SelectMany(j1 => junctions.Where(j2 => j1 != j2)
+        .Select(j2 => new Connection(
+            From: j1,
+            To: j2,
+            Distance: Math.Sqrt(
+                Math.Pow(j1.X - j2.X, 2) +
+                Math.Pow(j1.Y - j2.Y, 2) +
+                Math.Pow(j1.Z - j2.Z, 2)))))
+    .OrderBy(d => d.Distance)
+    .ToList();
+
+var circuits = new List<List<Coordinate>>();
+
+for (var i = 0; i < 1000; i++)
+{
+    var connection = connections.ElementAt(i * 2);
+    List<Coordinate> fromCircuit = null;
+    List<Coordinate> toCircuit = null;
+    foreach (var circuit in circuits)
+    {
+        if (circuit.Contains(connection.From))
+        {
+            fromCircuit = circuit;
+        }
+
+        if (circuit.Contains(connection.To))
+        {
+            toCircuit = circuit;
+        }
+    }
+
+    if (fromCircuit != null && fromCircuit == toCircuit)
+    {
+        continue;
+    }
+
+    if (fromCircuit != null && toCircuit != null)
+    {
+        fromCircuit.AddRange(toCircuit);
+        circuits.Remove(toCircuit);
+        continue;
+    }
+
+    if (fromCircuit != null)
+    {
+        fromCircuit.Add(connection.To);
+        continue;
+    }
+
+    if (toCircuit != null)
+    {
+        toCircuit.Add(connection.From);
+        continue;
+    }
+
+    circuits.Add(new List<Coordinate>() { connection.From, connection.To });
+}
+
+var answer = circuits
+    .OrderByDescending(c => c.Count)
+    .Take(3)
+    .Aggregate(1, (acc, circuit) => acc * circuit.Count);
+
+Console.WriteLine($"Answer: {answer}");
+
+record Coordinate(int X, int Y, int Z);
+record Connection(Coordinate From, Coordinate To, double Distance);
